970d2b15035a0061c344fbb146b09791c9d7b087
sksat <sksat@ubuntu.o3w3oafqrnyujgjic2pcirqzfc.bx.internal.cloudapp.net>
2017-05-16 12:02:28 +0000

    test python script

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ pman=apt-get
 echo "install packages"
 $pman install python3 python3-pip
 
-echo "install python library"
+echo "install python library\n"
+echo "twitter(https://github.com/sixohsix/twitter)"
 pip3 install --upgrade twitter
